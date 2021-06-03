f2db9ea1d9e443ba07e7e621f9b8852af2b9c58e
dawenxi <june982@163.com>
2021-06-03 16:22:49 +0800

    提交大事件

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,6 +1,7 @@
 $(function () {
   var layer = layui.layer;
   var form = layui.form;
+  var laypage = layui.laypage;
   var q = {
     pagenum: 1, // 页码值，默认请求第一页的数据
     pagesize: 2, // 每页显示几条数据，默认每页显示2条
@@ -19,6 +20,7 @@ $(function () {
         // console.log(res);
         var htmlStr = template("tpl-table", res);
         $("tbody").html(htmlStr);
+        rederList(res.total);
       },
     });
   }
@@ -66,4 +68,48 @@ $(function () {
   function padZero(n) {
     return n > 9 ? n : "0" + n;
   }
+
+  function rederList(total) {
+    // 定义渲染分页的方法
+
+    // 调用 laypage.render() 方法来渲染分页的结构
+    laypage.render({
+      elem: "pageList", // 分页容器的 Id
+      count: total, // 总数据条数
+      limit: q.pagesize, // 每页显示几条数据
+      layout: ["count", "limit", "prev", "page", "next", "skip"],
+      limits: [2, 3, 6, 8],
+      curr: q.pagenum, // 设置默认被选中的分页
+      //obj（当前分页的所有选项值）、first（是否首次，一般用于初始加载的判断）
+      jump: function (obj, first) {
+        q.pagenum = obj.curr;
+        q.pagesize = obj.limit;
+        if (!first) {
+          initTable();
+        }
+      },
+    });
+  }
+
+  //删除按钮点击事件
+  $("tbody").on("click", "#btnDel", function () {
+    layer.confirm("确定删除?", { icon: 3, title: "提示" }, function (index) {
+      //do something
+      var leng = $("#btnDel").length;
+      var id = $(this).attr("data-id");
+      $.ajax({
+        type: "get",
+        url: "/my/article/delete/" + id,
+        success: function (res) {
+          if (res.status !== 0) return layer.msg("删除失败");
+          layer.msg("删除成功");
+          initTable();
+          if (leng === 1) {
+            q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1;
+          }
+          layer.close(index);
+        },
+      });
+    });
+  });
 });
